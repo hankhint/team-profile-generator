43ae2abf4226bc7b5ff9f164ff508839d627b0f9
HankHint <hankhint@gmail.com>
2022-02-22 15:37:17 -0500

    added engineer HTML template

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,30 +1,5 @@
 // Generate HTML for Manager
 
-/* const genManager = function (manager) {
-    return `
-    <div class="card column is-one-third">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left">
-            <figure class="">
-            <i class="fas fa-user-tie is-2"></i>
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-4">${manager.name}</p>
-            <a class="subtitle is-6" href="mailto:${manager.email}" target="_blank">${manager.email}</a>
-            <p class="subtitle is-6">${manager.getRole()}</p>
-          </div>
-        </div>
-        <div class="content">
-          <span>ID: ${manager.id}</span>
-          <span>Office Number: ${manager.officeNumber}</span>
-        </div>
-      </div>
-    </div>
-    `;
-  }; */
-
 const generateManager = function (manager) {
   return `
   <div class="row">
@@ -32,9 +7,6 @@ const generateManager = function (manager) {
     <div class="card blue-grey darken-1">
       <div class="card-content white-text">
         <span class="card-title">${manager.name}</span>
-        <a class="subtitle is-6" href="mailto:${
-          manager.email
-        }" target="_blank">${manager.email}</a>
         <p>${manager.getRole()}</p>
         <div>
         <span>ID: ${manager.id}</span>
@@ -49,13 +21,33 @@ const generateManager = function (manager) {
     </div>
   </div>
 </div>
-    `;
+ `;
 };
 
 // Generate HTML for Engineer
 const generateEngineer = function (engineer) {
   return `
-    <p>engineer card</p>
+
+  <div class="row">
+  <div class="col s12 m6">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">${engineer.name}</span>
+        <a class="subtitle is-6" href="mailto:${
+          engineer.email
+        }" target="_blank">Send ${engineer.email} an email</a>
+        <p>${engineer.getRole()}</p>
+        <div>
+        <span>ID: ${engineer.id}</span>
+        <span>Github ID: ${engineer}</span>
+      </div>    
+        </div>
+      <div class="card-action">
+      <a href="https://github.com/${engineer.github}" target="_blank">GitHub: https://github.com/${engineer.github}</a>
+      </div>
+    </div>
+  </div>
+</div>
     `;
 };
 
