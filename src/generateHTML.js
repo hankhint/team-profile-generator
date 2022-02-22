d1ea4da53f259c2982f07339f23cb60dfceeadb0
HankHint <hankhint@gmail.com>
2022-02-22 15:24:53 -0500

    added manager HTML template

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,17 +1,50 @@
 // Generate HTML for Manager
+
+/* const genManager = function (manager) {
+    return `
+    <div class="card column is-one-third">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="">
+            <i class="fas fa-user-tie is-2"></i>
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">${manager.name}</p>
+            <a class="subtitle is-6" href="mailto:${manager.email}" target="_blank">${manager.email}</a>
+            <p class="subtitle is-6">${manager.getRole()}</p>
+          </div>
+        </div>
+        <div class="content">
+          <span>ID: ${manager.id}</span>
+          <span>Office Number: ${manager.officeNumber}</span>
+        </div>
+      </div>
+    </div>
+    `;
+  }; */
+
 const generateManager = function (manager) {
   return `
   <div class="row">
   <div class="col s12 m6">
     <div class="card blue-grey darken-1">
       <div class="card-content white-text">
-        <span class="card-title">Card Title</span>
-        <p>I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively.</p>
-      </div>
+        <span class="card-title">${manager.name}</span>
+        <a class="subtitle is-6" href="mailto:${
+          manager.email
+        }" target="_blank">${manager.email}</a>
+        <p>${manager.getRole()}</p>
+        <div>
+        <span>ID: ${manager.id}</span>
+        <span>Office Number: ${manager.officeNumber}</span>
+      </div>    
+        </div>
       <div class="card-action">
-        <a href="#">This is a link</a>
-        <a href="#">This is a link</a>
+        <a href="mailto:${manager.email}" target="_blank">Send ${
+    manager.name
+  } an email</a>
       </div>
     </div>
   </div>
@@ -59,7 +92,11 @@ const generateTeamPage = function (employeeCards) {
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <body>
     </body>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+    </html>
     `;
 };
 
