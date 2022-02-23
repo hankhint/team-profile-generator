f18570689ec1f612f4c5ceef2261ffa0f902facd
HankHint <hankhint@gmail.com>
2022-02-23 10:20:22 -0500

    adding tests to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,20 @@
 const Employee = require('../lib/Employee');
 
+//creates a new employee object to test name, id number, and email
+test('creates an Employee object', () => {
+    const employee = new Employee('Tom', '200', 'Tom@mail.com')
+
+    expect(employee.name).toEqual(expect.any(Number));
+    expect(employee.id).toEqual);
+    expect(employee.email).toBe('Tom@mail.com')
+})
 // name
 
 // id
 
 // email
+
+Te
 
 // getName()
 
