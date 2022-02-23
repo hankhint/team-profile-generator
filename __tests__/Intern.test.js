8dce20ca47a3daef7ae192a286ab79da04ad8367
HankHint <hankhint@gmail.com>
2022-02-23 14:23:21 -0500

    intern test suite debugged and passes

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,28 @@
+//link to file with methods that are being tested
 const Intern = require('../lib/Intern');
 
-// extend employee
 
-//school
+//link to file with methods that are being tested
+// extend employee to Intern
+test('creates intern', () => {
+  const intern = new Intern("Mobby", 3000, "Mobby@cry.com", "havad");
 
-// getSchool()
+  expect(intern.name).toBe("Mobby");
+  expect(intern.id).toBe(3000);
+  expect(intern.email).toBe("Mobby@cry.com");
+  expect(intern.school).toBe("havad");
+});
+
+// getSchool() returns "havad"
+test('gets intern school', () => {
+  const intern = new Intern('Mobby', 3000, 'Mobby@cry.com', 'havad');
+
+  expect(intern.getSchool()).toBe('havad');
+});
 
 // getRole() // Overridden to return 'Intern'
+test('gets Intern Role', () => {
+  const intern = new Intern('Mobby', 3000, 'Mobby@cry.com', 'havad');
+
+  expect(intern.getRole()).toBe('Intern');
+});
