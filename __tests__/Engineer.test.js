341ea10ee80464efa9a11e14e4e4bd354cce7af9
HankHint <hankhint@gmail.com>
2022-02-23 11:22:33 -0500

    added engineer test suite, tests pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,10 +1,27 @@
 //link to file with methods that are being tested
 const Engineer = require('../lib/Engineer');
 
-// extend employee
+// extend employee to Engineer
+test('creates engineer', () => {
+    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
+
+    expect(engineer.name).toBe('Lobby');
+    expect(engineer.id).toBe(20);
+    expect(engineer.email).toBe('Lobby@cry.com');
+    expect(engineer.github).toBe('hastwo');
+});
+
 
 // github // GitHub username
-
-// getGithub()
+test('gets Engineer github username', () => {
+    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
+  
+    expect(engineer.getGithub()).toBe('hastwo');
+  });
 
 // getRole() // Overridden to return 'Engineer'
+test('gets Engineer Role', () => {
+    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
+  
+    expect(engineer.getRole()).toBe('Engineer');
+  });
