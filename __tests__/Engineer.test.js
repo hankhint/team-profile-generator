6a2db8a10ad3da489e8f3a221dbe2683ebdd34df
HankHint <hankhint@gmail.com>
2022-02-23 13:54:24 -0500

    fixed formatting with engineer and manager test suites

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,27 +1,26 @@
 //link to file with methods that are being tested
-const Engineer = require('../lib/Engineer');
+const Engineer = require("../lib/Engineer");
 
 // extend employee to Engineer
-test('creates engineer', () => {
-    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
+test("creates engineer", () => {
+  const engineer = new Engineer("Lobby", 20, "Lobby@cry.com", "hastwo");
 
-    expect(engineer.name).toBe('Lobby');
-    expect(engineer.id).toBe(20);
-    expect(engineer.email).toBe('Lobby@cry.com');
-    expect(engineer.github).toBe('hastwo');
+  expect(engineer.name).toBe("Lobby");
+  expect(engineer.id).toBe(20);
+  expect(engineer.email).toBe("Lobby@cry.com");
+  expect(engineer.github).toBe("hastwo");
 });
 
-
 // github // GitHub username
-test('gets Engineer github username', () => {
-    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
-  
-    expect(engineer.getGithub()).toBe('hastwo');
-  });
+test("gets Engineer github username", () => {
+  const engineer = new Engineer("Lobby", 20, "Lobby@cry.com", "hastwo");
+
+  expect(engineer.getGithub()).toBe("hastwo");
+});
 
 // getRole() // Overridden to return 'Engineer'
-test('gets Engineer Role', () => {
-    const engineer = new Engineer('Lobby', 20, 'Lobby@cry.com', 'hastwo');
-  
-    expect(engineer.getRole()).toBe('Engineer');
-  });
+test("gets Engineer Role", () => {
+  const engineer = new Engineer("Lobby", 20, "Lobby@cry.com", "hastwo");
+
+  expect(engineer.getRole()).toBe("Engineer");
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,27 +1,26 @@
 //link to file with methods that are being tested
-const Manager = require('../lib/Manager');
+const Manager = require("../lib/Manager");
 
 //extend employee to create manager
-test('creates manager', () => {
-    const manager = new Manager('Sobby', 420, 'Sobby@cry.com', 666);
+test("creates manager", () => {
+  const manager = new Manager("Sobby", 420, "Sobby@cry.com", 666);
 
-    expect(manager.name).toBe('Sobby');
-    expect(manager.id).toBe(420);
-    expect(manager.email).toBe('Sobby@cry.com');
-    expect(manager.officeNumber).toBe(666);
+  expect(manager.name).toBe("Sobby");
+  expect(manager.id).toBe(420);
+  expect(manager.email).toBe("Sobby@cry.com");
+  expect(manager.officeNumber).toBe(666);
 });
 
-
 //officeNumber
-test('tests Manager office number', () => {
-    const manager = new Manager('Sobby', 420, 'Sobby@cry.com', 666);
+test("tests Manager office number", () => {
+  const manager = new Manager("Sobby", 420, "Sobby@cry.com", 666);
 
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+  expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // getRole() // Overridden to return 'Manager'
-test('gets Manager Role', () => {
-    const manager = new Manager('Sobby', 420, 'Sobby@cry.com', 666);
-  
-    expect(manager.getRole()).toBe('Manager');
-  });
+test("gets Manager Role", () => {
+  const manager = new Manager("Sobby", 420, "Sobby@cry.com", 666);
+
+  expect(manager.getRole()).toBe("Manager");
+});
